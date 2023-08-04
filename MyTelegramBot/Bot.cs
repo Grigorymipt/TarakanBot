7aed3bc1f5fd7616eaa7fdc34eab2d6bc9f499e9
Grigorymipt <koveshnikov104@gmail.com>
2023-08-04 16:54:31 +0300

    https

--- MyTelegramBot/Bot.cs
+++ MyTelegramBot/Bot.cs
@@ -82,7 +82,7 @@ public class Bot {
         using CancellationTokenSource cts = new CancellationTokenSource();
         //TODO: remove hardcode
         _botClient.SetWebhookAsync(
-            url: "https://testfortestingandtestingfortest.ru:443/GetUpdates",
+            url: "https://somegreentests.ru/GetUpdates",
             // ipAddress: "62.113.98.40",
             maxConnections: default,
             allowedUpdates: default,
