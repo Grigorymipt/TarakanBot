ed7261d81359746e3340c0a81154f8bdedf98b08
Grigorymipt <koveshnikov104@gmail.com>
2023-08-04 18:13:04 +0300

    https

--- MyTelegramBot/WebHook.cs
+++ MyTelegramBot/WebHook.cs
@@ -11,6 +11,12 @@ public static class WebHook
     public static async Task UpdateReceive(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        // Setup Bot configuration
+        var botConfigurationSection = builder.Configuration.GetSection(BotConfiguration.Configuration);
+        builder.Services.Configure<BotConfiguration>(botConfigurationSection);
+        var botConfiguration = botConfigurationSection.Get<BotConfiguration>();
+
         // Add services to the container.
         builder.Services.AddScoped<Bot>();
         builder.Services.AddControllers().AddNewtonsoftJson();
@@ -18,7 +24,12 @@ public static class WebHook
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        
+        builder.Services.AddHttpClient("telegram_bot_client")
+                .AddTypedClient<ITelegramBotClient>(httpClient =>
+                {
+                    TelegramBotClientOptions options = new(Environment.GetEnvironmentVariable(Config.BotToken));
+                    return new TelegramBotClient(options, httpClient);
+                });
         builder.WebHost.ConfigureKestrel(serverOptions =>
         {
             serverOptions.Listen(IPAddress.Any, 8080, options =>
@@ -46,5 +57,15 @@ public static class WebHook
         app.MapControllers();
         app.Run();
     }
+    public class BotConfiguration
+    {
+        public static readonly string Configuration = "BotConfiguration";
+
+        public string BotToken { get; init; } = default!;
+        public string HostAddress { get; init; } = default!;
+        public string Route { get; init; } = default!;
+        public string SecretToken { get; init; } = default!;
+    }
 }
+
 
