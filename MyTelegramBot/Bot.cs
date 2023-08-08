b5cd7260903c35e1e064c20eed76e6e5fa22f48a
Grigorymipt <koveshnikov104@gmail.com>
2023-08-08 10:32:47 +0300

    channelName

--- MyTelegramBot/Bot.cs
+++ MyTelegramBot/Bot.cs
@@ -97,7 +97,7 @@ public class Bot {
             secretToken: null,
             cancellationToken: cts.Token
         );
-        ReceiverOptions receiverOptions = new ReceiverOpti ons
+        ReceiverOptions receiverOptions = new ReceiverOptions
         {
             AllowedUpdates = {},
         };
